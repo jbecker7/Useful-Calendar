3cf509d8bf5e12975ad4ccc09e982bed614e666f
Jonathan <jbecker24@cmc.edu>
2023-06-19 18:37:37 -0700

    improved the UI

--- script.ts
+++ script.ts
@@ -1,11 +1,16 @@
+const waitTimeElement: HTMLElement | null = document.getElementById("wait-time");
+const startTime: number = Date.now();
+
 function formatDate(dateString: string): string {
     const options: Intl.DateTimeFormatOptions = { year: 'numeric', month: '2-digit', day: '2-digit' };
     return new Date(dateString).toLocaleDateString(undefined, options);
 }
 
+const waittime: number = Math.floor(Math.random() * 10000) + 20000;
+
 document.querySelector('jsuites-calendar')?.addEventListener('onchange', function(e: Event) {
     const selectedDate = formatDate((e.target as HTMLInputElement).value);
-    const spinnerElement = document.getElementById("spinner")!;
+    const hourglassElement = document.getElementById("hourglass")!;
     const resultElement = document.getElementById("result")!;
     const gameOverElement = document.getElementById("game-over")!;
     const celebrationElement = document.getElementById("celebration")!;
@@ -25,23 +30,23 @@ document.querySelector('jsuites-calendar')?.addEventListener('onchange', functio
 
     const showGameOverScreen = () => {
         stopLoadingAudio(loadingAudio);
-        spinnerElement.style.display = "none";
+        hourglassElement.style.display = "none";
         resultElement.style.display = "none";
         gameOverElement.style.display = "block";
-        celebrationElement.style.display = "none"; 
+        celebrationElement.style.display = "none";
         gameOverAudio.play();
     };
 
     const showCelebrationScreen = () => {
         stopLoadingAudio(loadingAudio);
-        spinnerElement.style.display = "none";
+        hourglassElement.style.display = "none";
         resultElement.style.display = "none";
         gameOverElement.style.display = "none";
         celebrationElement.style.display = "block";
         celebrationAudio.play();
     };
 
-    spinnerElement.style.display = "block";
+    hourglassElement.style.display = "block";
     resultElement.style.display = "none";
     gameOverElement.style.display = "none";
     celebrationElement.style.display = "none";
@@ -53,21 +58,51 @@ document.querySelector('jsuites-calendar')?.addEventListener('onchange', functio
             .then(response => response.text())
             .then(currentDate => {
                 stopLoadingAudio(loadingAudio);
-                spinnerElement.style.display = "none";
-                if(currentDate === selectedDate) {
+                hourglassElement.style.display = "none";
+                if (currentDate === selectedDate) {
                     resultElement.style.display = "block";
                     resultElement.textContent = "Selected date is today's date.";
-                    showCelebrationScreen(); 
+                    showCelebrationScreen();
                 } else {
-                    resultElement.style.display = "none"; 
+                    resultElement.style.display = "none";
                     showGameOverScreen();
                 }
             })
             .catch(error => {
                 stopLoadingAudio(loadingAudio);
-                spinnerElement.style.display = "none";
+                hourglassElement.style.display = "none";
                 resultElement.style.display = "block";
                 resultElement.textContent = 'Error: ' + error;
             });
-    }, 10000);
+    }, waittime);
+    console.log('waittime: ' + waittime);
+
+    // Update wait time element
+    if (waitTimeElement) {
+        waitTimeElement.textContent = "Wait Time: " + (waittime / 1000) + " seconds";
+        waitTimeElement.style.display = "block";
+    }
 });
+
+// Create a separate block to display elapsed time
+const elapsedTimeElement = document.createElement("div");
+elapsedTimeElement.id = "elapsed-time";
+elapsedTimeElement.style.position = "fixed";
+elapsedTimeElement.style.top = "10px";
+elapsedTimeElement.style.right = "10px";
+elapsedTimeElement.style.backgroundColor = "FFF";
+elapsedTimeElement.style.color = "#FFF";
+elapsedTimeElement.style.padding = "10px";
+elapsedTimeElement.style.border = "2px solid #FFF";
+document.body.appendChild(elapsedTimeElement);
+
+// Update elapsed time
+setInterval(function() {
+    const elapsedTime = Date.now() - startTime;
+    elapsedTimeElement.textContent = "You have been on this site for " + Math.floor(elapsedTime / 1000) + " seconds";
+}, 100);
+
+if (waitTimeElement) {
+    waitTimeElement.textContent = "Current Wait Time: " + waittime / 1000 + " seconds";
+    waitTimeElement.style.display = "block";
+}
